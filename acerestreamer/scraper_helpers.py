3e3436e579c924ce9fb03e39ef8d0428e22ca23c
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-06-14 20:03:34 +0800

    maybe fix

--- acerestreamer/scraper_helpers.py
+++ acerestreamer/scraper_helpers.py
@@ -47,7 +47,7 @@ def get_streams_as_iptv(streams: list[FlatFoundAceStream], base_url_hls: str) ->
     for stream in streams:
         logger.debug(stream)
         if stream.quality > 0:
-            m3u8_content += f"#EXTINF:-1 ,{stream.title}\n"
+            m3u8_content += f'#EXTINF:-1 infohash="{stream.ace_id}",{stream.title}\n'
             m3u8_content += f"{base_url_hls}{stream.ace_id}\n"
 
     return m3u8_content
