8b42602f42cb127e9f60eb269f56b8ae3f2cc707
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-06-27 16:43:07 +0800

    cleanup

--- acerestreamer/utils/__init__.py
+++ acerestreamer/utils/__init__.py
@@ -1,11 +1,11 @@
 """Utility functions and helpers."""
 
-from acerestreamer.utils.helpers import (
+from .helpers import (
     check_valid_ace_id,
     slugify,
 )
-from acerestreamer.utils.html_snippets import get_header_snippet
-from acerestreamer.utils.m3u import replace_m3u_sources
+from .html_snippets import get_header_snippet
+from .m3u import replace_m3u_sources
 
 __all__ = [
     "check_valid_ace_id",
