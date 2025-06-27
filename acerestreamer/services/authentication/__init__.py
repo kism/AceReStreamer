92ce3324771b2420ff542c73599ae4b0b171308d
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-06-27 13:13:25 +0800

    wip

--- acerestreamer/services/authentication/__init__.py
+++ acerestreamer/services/authentication/__init__.py
@@ -1,11 +1,10 @@
 """Authentication service for Ace ReStreamer."""
 
 from .allow_list import AllowList
-from .helpers import assumed_auth_failure, get_ip_from_request, is_ip_allowed
+from .helpers import assumed_auth_failure, get_ip_from_request
 
 __all__ = [
     "AllowList",
     "assumed_auth_failure",
     "get_ip_from_request",
-    "is_ip_allowed",
 ]
