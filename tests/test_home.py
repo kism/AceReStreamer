bed5838301c4062fefa8053c5cc5cde5e08269e9
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-06-27 15:02:48 +0800

    better tests

--- tests/test_home.py
+++ tests/test_home.py
@@ -17,7 +17,7 @@ def test_static_js_exists(client):
     assert response.status_code == HTTPStatus.OK
 
 
-@pytest.mark.parametrize("path", ["/login", "/stream"])
+@pytest.mark.parametrize("path", ["/login", "/stream", "/info/guide", "/info/iptv", "/info/api"])
 def test_login(client, path):
     """Test OK response with no password set."""
     response = client.get(path)
@@ -37,3 +37,39 @@ def test_password(client, app):
     )
     response = client.get("/stream")
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/api/authenticate",
+        "/api/streams/flat",
+        "/api/streams/by_source",
+        "/api/sources",
+        "/api/sources/flat",
+        "/api/streams/health",
+        "/api/ace_pool",
+        "/api/epgs",
+    ],
+)
+def test_api_basic(client, path):
+    """Test OK response with no password set."""
+    response = client.get(path)
+    assert response.status_code == HTTPStatus.OK
+    assert response.content_type == "application/json"
+    assert b"{" in response.data or b"[" in response.data
+
+
+def test_misc_endpoints(client):
+    """Test some miscellaneous endpoints."""
+    response = client.get("/favicon.ico")
+    assert response.status_code == HTTPStatus.OK
+    assert response.content_type == "image/x-icon"
+
+    response = client.get("/epg")
+    assert response.status_code == HTTPStatus.OK
+    assert response.content_type == "application/xml; charset=utf-8"
+
+    response = client.get("/iptv")
+    assert response.status_code == HTTPStatus.OK
+    assert response.content_type == "application/vnd.apple.mpegurl"
