adc6849f4f007dd485b8a0d133bca52a0d542bdb
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-02-09 14:59:43 +0800

    fix

--- scripts/run-playwright.sh
+++ scripts/run-playwright.sh
@@ -2,20 +2,16 @@
 
 set -e
 
-ACERE_FIRST_SUPERUSER_USERNAME="admin"
+export ACERE_FIRST_SUPERUSER_USERNAME="admin"
 ACERE_FIRST_SUPERUSER_PASSWORD="$(openssl rand -base64 16)"
-DOTENV_FILE="$(pwd)/frontend/.env_playwright"
-VITE_API_URL="http://acerestreamer-backend-playwright:5100"
-
-echo "ACERE_FIRST_SUPERUSER_USERNAME=${ACERE_FIRST_SUPERUSER_USERNAME}" >"${DOTENV_FILE}"
-echo "ACERE_FIRST_SUPERUSER_PASSWORD=${ACERE_FIRST_SUPERUSER_PASSWORD}" >>"${DOTENV_FILE}"
-echo "VITE_API_URL=${VITE_API_URL}" >>"${DOTENV_FILE}"
+export ACERE_FIRST_SUPERUSER_PASSWORD
+export VITE_API_URL="http://acerestreamer-backend-playwright:5100"
 
 # Backend
 docker buildx build --file docker/Dockerfile.backend . -t acerestreamer-backend:latest
 docker buildx build --file docker/Dockerfile.frontend --build-arg VITE_API_URL=${VITE_API_URL} . -t acerestreamer-frontend:latest
 
 docker compose -f frontend/docker-compose.playwright.yml down --volumes --remove-orphans
-docker compose -f frontend/docker-compose.playwright.yml --env-file "${DOTENV_FILE}" up --build --abort-on-container-exit
+docker compose -f frontend/docker-compose.playwright.yml up --build --abort-on-container-exit
 
 docker logs acerestreamer-playwright-tests
