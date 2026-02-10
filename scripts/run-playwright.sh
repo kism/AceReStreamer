8bacfc1377edba5872b4dbfc648310f494cc362b
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-02-10 12:17:51 +0800

    wip

--- scripts/run-playwright.sh
+++ scripts/run-playwright.sh
@@ -8,8 +8,8 @@ export ACERE_FIRST_SUPERUSER_PASSWORD
 export VITE_API_URL="http://acerestreamer-backend-playwright:5100"
 
 # Backend
-docker buildx build --file docker/Dockerfile.backend . -t acerestreamer-backend:latest
-docker buildx build --file docker/Dockerfile.frontend --build-arg VITE_API_URL=${VITE_API_URL} . -t acerestreamer-frontend:latest
+docker buildx build --quiet --file docker/Dockerfile.backend . -t acerestreamer-backend:latest
+docker buildx build --quiet --file docker/Dockerfile.frontend --build-arg VITE_API_URL=${VITE_API_URL} . -t acerestreamer-frontend:latest
 
 docker compose -f frontend/docker-compose.playwright.yml down --volumes --remove-orphans
 docker compose -f frontend/docker-compose.playwright.yml up --build --abort-on-container-exit
