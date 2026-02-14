94195c8f2f72acce07afc3817c19e77e56f2333c
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-02-14 13:05:43 +0800

    a

--- tests/services/scraper/test_scraper_iptv_infohash.py
+++ tests/services/scraper/test_scraper_iptv_infohash.py
@@ -74,21 +74,21 @@ async def test_site1(scraper: IPTVStreamScraper) -> None:
         assert stream.infohash is not None
 
 
-# async def test_site2(scraper: IPTVStreamScraper, caplog: pytest.LogCaptureFixture) -> None:
-#     """Test scraping from site2.html."""
-#     playlist_name = "playlist_infohash_2.m3u8"
-#     site_config = _get_test_sites()[playlist_name]
+async def test_site2(scraper: IPTVStreamScraper, caplog: pytest.LogCaptureFixture) -> None:
+    """Test scraping from site2.html."""
+    playlist_name = "playlist_infohash_2.m3u8"
+    site_config = _get_test_sites()[playlist_name]
 
-#     with caplog.at_level("TRACE", logger="acere.services.scraper.iptv"):
-#         streams = await scraper.scrape_iptv_playlists([site_config])
+    with caplog.at_level("TRACE", logger="acere.services.scraper.iptv"):
+        streams = await scraper.scrape_iptv_playlists([site_config])
 
-#     assert len(streams) == 2
+    assert len(streams) == 2
 
-#     for stream in streams:
-#         common_title_check(stream.title)
-#         assert "ChannelInfo" in stream.tvg_id
-#         assert "ChannelInfo" in stream.title
-#         assert stream.group_title == "General"
-#         assert stream.tvg_logo is not None
-#         assert "ChannelInfo" in stream.tvg_logo
-#         assert stream.infohash is not None
+    for stream in streams:
+        common_title_check(stream.title)
+        assert "ChannelInfo" in stream.tvg_id
+        assert "ChannelInfo" in stream.title
+        assert stream.group_title == "General"
+        assert stream.tvg_logo is not None
+        assert "ChannelInfo" in stream.tvg_logo
+        assert stream.infohash is not None
