a831775e6ac287642b74aa0e8c98528305d57b27
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-02-15 12:13:52 +0800

    fix

--- TODO.md
+++ TODO.md
@@ -1,12 +1,5 @@
 # TODO
 
-- Document scraper mode
-- Document reset password tool
-- Document epg tool
-- Better TOC
-- fixture, picture
-  @pytest.fixture
-  async def scraper(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> IPTVStreamScraper:
 - oddball casts
 - tests/core/config/test_urls.py
 - generated_frontend_paths only works if VITE_API_URL is set
--- tests/core/config/test_urls.py
+++ tests/core/config/test_urls.py
@@ -1,7 +1,10 @@
+import pytest
+
 from acere.core.config import AceReStreamerConf
+from acere.core.config.app import AppConf
 
 
-def test_urls_as_strings() -> None:
+def test_default_urls_as_strings() -> None:
     """Test that URL configurations are properly converted to strings."""
     settings = AceReStreamerConf()
 
@@ -10,7 +13,24 @@ def test_urls_as_strings() -> None:
     assert isinstance(ace_address_str, str)
     assert ace_address_str.endswith("/")
 
-def test_external_url_as_string() -> None:
+
+@pytest.mark.parametrize(
+    ("url", "expected"),
+    [
+        ("http://localhost:6878/", "http://localhost:6878/"),
+        ("http://localhost:6878", "http://localhost:6878/"),
+    ],
+)
+def test_strip_trailing_slash(url: str, expected: str) -> None:
+    """Test that the strip_trailing_slash function correctly removes trailing slashes."""
+    app_config = AppConf(
+        ace_address=url,  # type: ignore[arg-type]
+    )
+
+    assert app_config.ace_address.encoded_string() == expected
+
+
+def test_default_external_url_as_string() -> None:
     """Test that EXTERNAL_URL is properly converted to a string."""
     settings = AceReStreamerConf()
 
@@ -19,3 +39,22 @@ def test_external_url_as_string() -> None:
     assert isinstance(external_url_str, str)
     assert not external_url_str.endswith("/")
 
+
+@pytest.mark.parametrize(
+    ("url", "expected"),
+    [
+        ("http://ace.pytest.internal:8000/", "http://ace.pytest.internal:8000"),
+        ("http://ace.pytest.internal:8000", "http://ace.pytest.internal:8000"),
+        ("http://ace.pytest.internal", "http://ace.pytest.internal"),
+        ("http://ace.pytest.internal/", "http://ace.pytest.internal"),
+        ("https://ace.pytest.internal", "https://ace.pytest.internal"),
+        ("https://ace.pytest.internal/", "https://ace.pytest.internal"),
+    ],
+)
+def test_external_url_strip_trailing_slash(url: str, expected: str) -> None:
+    """Test that the EXTERNAL_URL configuration correctly removes trailing slashes."""
+    settings = AceReStreamerConf(
+        EXTERNAL_URL=url,
+    )
+
+    assert expected == settings.EXTERNAL_URL
